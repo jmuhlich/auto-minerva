2ae874a6a60ddf4c6f9240ce4f20e5cb8aeb98ea
Jeremy Muhlich <jmuhlich@bitflood.org>
2021-11-12 16:28:09 -0500

    Use intersection of gaussians instead of lower mean + stdev
    The idea is that the point where the (weighted) gaussians for the upper
    two modes intersect is the cutoff where a pixel is equally likely to be
    foreground as background.

--- story.py
+++ story.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 import ome_types
+import scipy.stats
 import sklearn.mixture
 import sys
 import threadpoolctl
@@ -20,14 +21,20 @@ def auto_threshold(img):
     gmm = sklearn.mixture.GaussianMixture(3, max_iter=1000, tol=1e-6)
     gmm.fit(img_log.reshape((-1,1)))
     means = gmm.means_[:, 0]
-    covars = gmm.covariances_[:, 0, 0]
     _, i1, i2 = np.argsort(means)
+    mean1, mean2 = means[[i1, i2]]
+    std1, std2 = gmm.covariances_[[i1, i2], 0, 0] ** 0.5
 
-    vmin, vmax = means[[i1, i2]] + covars[[i1, i2]] ** 0.5 * 2
-    if vmin >= means[i2]:
-        vmin = means[i2] + covars[i2] ** 0.5 * -1
-    vmin = max(np.exp(vmin), img.min(), 0)
-    vmax = min(np.exp(vmax), img.max())
+    x = np.linspace(mean1, mean2, 50)
+    y1 = scipy.stats.norm(mean1, std1).pdf(x) * gmm.weights_[i1]
+    y2 = scipy.stats.norm(mean2, std2).pdf(x) * gmm.weights_[i2]
+
+    lmax = mean2 + 2 * std2
+    lmin = x[np.argmin(np.abs(y1 - y2))]
+    if lmin >= mean2:
+        lmin = mean2 - 2 * std2
+    vmin = max(np.exp(lmin), img.min(), 0)
+    vmax = min(np.exp(lmax), img.max())
 
     return vmin, vmax
 
